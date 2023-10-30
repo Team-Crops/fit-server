46eefafbd80b8e4302c8ba41ba51c0157e05e258
유서린 <sprout3082@gmail.com>
2023-10-30 19:52:01 +0900

    [init] init commit

---
+++ src/main/java/org/crops/fitserver/FitserverApplication.java
@@ -0,0 +1,13 @@
+package org.crops.fitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitserverApplication.class, args);
+	}
+
+}
