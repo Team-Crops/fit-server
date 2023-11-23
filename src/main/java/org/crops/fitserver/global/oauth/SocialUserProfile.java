668fd70edab3fa6af6053b3863d5a4dc53cdd7e5
유서린 <sprout3082@gmail.com>
2023-11-23 14:51:35 +0900

    #2 [feat] Social User Profile 도메인모델 추가

---
+++ src/main/java/org/crops/fitserver/global/oauth/SocialUserProfile.java
@@ -0,0 +1,15 @@
+package org.crops.fitserver.global.oauth;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SocialUserProfile {
+
+	private Long id;
+}
