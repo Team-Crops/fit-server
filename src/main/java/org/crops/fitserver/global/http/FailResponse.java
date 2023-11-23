4eb8488f560854eb43f185737c1953e0e2509d3c
유서린 <sprout3082@gmail.com>
2023-11-23 14:52:48 +0900

    #2 [feat] Fail Response  추가

---
+++ src/main/java/org/crops/fitserver/global/http/FailResponse.java
@@ -0,0 +1,13 @@
+package org.crops.fitserver.global.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FailResponse {
+
+	private int code;
+
+	private String message;
+}
