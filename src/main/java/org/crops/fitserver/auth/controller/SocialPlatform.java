071eecd07cb9805a132bc58d9f11992a861c9d34
유서린 <sprout3082@gmail.com>
2023-11-23 14:50:33 +0900

    #2 [feat] Social Platform Entity추

--- src/main/java/org/crops/fitserver/auth/controller/SocialPlatform.java
+++ src/main/java/org/crops/fitserver/auth/controller/SocialPlatform.java
@@ -1,8 +1,22 @@
 package org.crops.fitserver.auth.controller;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SocialPlatform {
 
-	KAKAO,
-	GOOGLE,
+	KAKAO("kakao"),
+	GOOGLE("google"),
 	;
+	private final String name;
+
+	public static SocialPlatform of(String name) {
+		for (SocialPlatform socialPlatform : SocialPlatform.values()) {
+			if (socialPlatform.name.equals(name)) {
+				return socialPlatform;
+			}
+		}
+		throw new IllegalArgumentException("존재하지 않는 소셜 플랫폼입니다.");
+	}
 }
