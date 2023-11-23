bfd2b19650bde5c8fb71cad82f6707752ec3cf5a
유서린 <sprout3082@gmail.com>
2023-11-23 14:52:03 +0900

    #2 [feat] UserRepository  추가

---
+++ src/main/java/org/crops/fitserver/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.crops.fitserver.user.repository;
+
+import java.util.Optional;
+import org.crops.fitserver.user.domain.User;
+import org.springframework.data.repository.Repository;
+
+public interface UserRepository extends Repository<User, Long>, UserRepositoryCustom {
+
+	Optional<User> findById(Long id);
+}
