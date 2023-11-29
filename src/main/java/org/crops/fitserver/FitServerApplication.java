c5ebb5787c6eb504494008e287521043a0abdf14
유서린 <sprout3082@gmail.com>
2023-11-29 22:30:18 +0900

    #2 [refactor] FeignConfig 추가

--- src/main/java/org/crops/fitserver/FitServerApplication.java
+++ src/main/java/org/crops/fitserver/FitServerApplication.java
@@ -2,14 +2,11 @@ package org.crops.fitserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class FitServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitServerApplication.class, args);
 	}
-
 }
