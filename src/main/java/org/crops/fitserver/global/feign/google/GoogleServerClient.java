0662fad6a62e1032361f7423c944fb2a88c44341
유서린 <sprout3082@gmail.com>
2023-11-30 22:28:38 +0900

    #2 [feat] Google Feign Client 추가

---
+++ src/main/java/org/crops/fitserver/global/feign/google/GoogleServerClient.java
@@ -0,0 +1,17 @@
+package org.crops.fitserver.global.feign.google;
+
+import org.crops.fitserver.global.feign.GoogleSocialUserProfile;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "googleServerClient", url = "https://www.googleapis.com")
+public interface GoogleServerClient {
+
+	@PostMapping(value = "/oauth2/v3/userinfo", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	GoogleSocialUserProfile getUserInformation(
+			@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken
+	);
+}
