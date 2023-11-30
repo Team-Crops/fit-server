c092d407a2a41739b06522b8488e6315fb6dc741
유서린 <sprout3082@gmail.com>
2023-11-30 22:29:43 +0900

    #2 [feat] Google OAuth Service 추가

---
+++ src/main/java/org/crops/fitserver/auth/service/impl/GoogleOAuthServiceImpl.java
@@ -0,0 +1,64 @@
+package org.crops.fitserver.auth.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.crops.fitserver.global.feign.GoogleSocialUserProfile;
+import org.crops.fitserver.global.feign.OAuthToken;
+import org.crops.fitserver.global.feign.google.GoogleAuthServerClient;
+import org.crops.fitserver.global.feign.google.GoogleClientProperty;
+import org.crops.fitserver.global.feign.google.GoogleServerClient;
+import org.crops.fitserver.auth.service.OAuthService;
+import org.crops.fitserver.global.jwt.JwtProperty;
+import org.crops.fitserver.user.domain.SocialPlatform;
+import org.crops.fitserver.user.domain.SocialUserInfo;
+import org.crops.fitserver.user.domain.User;
+import org.crops.fitserver.user.repository.SocialUserInfoRepository;
+import org.crops.fitserver.user.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GoogleOAuthServiceImpl implements OAuthService {
+
+	private final JwtProperty jwtProperty;
+	private final GoogleAuthServerClient googleAuthServerClient;
+	private final GoogleServerClient googleServerClient;
+	private final GoogleClientProperty googleClientProperty;
+	private final SocialUserInfoRepository socialUserInfoRepository;
+	private final UserRepository userRepository;
+
+	@Override
+	public User socialUserLogin(
+			String redirectUri,
+			String authorizationCode) {
+		OAuthToken oAuthToken = googleAuthServerClient.getOAuth2AccessToken(
+				googleClientProperty.getContentType(),
+				googleClientProperty.getGrantType(),
+				googleClientProperty.getClientId(),
+				googleClientProperty.getClientSecret(),
+				redirectUri,
+				authorizationCode);
+		GoogleSocialUserProfile socialUserProfile = googleServerClient.getUserInformation(
+				jwtProperty.getBearerPrefix() + oAuthToken.getAccessToken());
+		String socialCode = SocialUserInfo.calculateSocialCode(
+				SocialPlatform.GOOGLE,
+				socialUserProfile.getSub());
+		SocialUserInfo socialUserInfo = socialUserInfoRepository
+				.findBySocialCode(socialCode)
+				.orElseGet(
+						() -> {
+							User newUser = userRepository.save(
+									User.newInstance());
+							return socialUserInfoRepository.save(
+									SocialUserInfo.newInstance(
+											newUser,
+											SocialPlatform.GOOGLE,
+											socialCode));
+						});
+		return socialUserInfo.getUser();
+	}
+
+	@Override
+	public String getLoginPageUrl() {
+		return googleClientProperty.getLoginPageUrl();
+	}
+}
