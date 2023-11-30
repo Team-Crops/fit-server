60d94deee34cd0ce6450d342a3d929932cf7125c
유서린 <sprout3082@gmail.com>
2023-11-30 22:27:57 +0900

    #2 [feat] Login page uri 조회 Facade

--- src/main/java/org/crops/fitserver/auth/facade/AuthFacade.java
+++ src/main/java/org/crops/fitserver/auth/facade/AuthFacade.java
@@ -1,9 +1,15 @@
 package org.crops.fitserver.auth.facade;
 
+import org.crops.fitserver.auth.facade.dto.SocialLoginPageResponse;
 import org.crops.fitserver.user.domain.SocialPlatform;
 import org.crops.fitserver.auth.facade.dto.TokenResponse;
 
 public interface AuthFacade {
 
-	TokenResponse socialLogin(String redirectUrl, String authorizationCode, SocialPlatform socialPlatform);
+	TokenResponse socialLogin(
+			String redirectUrl,
+			String authorizationCode,
+			SocialPlatform socialPlatform);
+
+	SocialLoginPageResponse getSocialLoginPageUrl(SocialPlatform socialPlatform);
 }
--- src/main/java/org/crops/fitserver/auth/facade/impl/AuthFacadeImpl.java
+++ src/main/java/org/crops/fitserver/auth/facade/impl/AuthFacadeImpl.java
@@ -1,12 +1,15 @@
 package org.crops.fitserver.auth.facade.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.crops.fitserver.auth.facade.dto.SocialLoginPageResponse;
 import org.crops.fitserver.auth.service.OAuthService;
 import org.crops.fitserver.auth.service.provider.OAuthServiceProvider;
+import org.crops.fitserver.global.jwt.JwtProvider;
+import org.crops.fitserver.global.jwt.TokenInfo;
 import org.crops.fitserver.user.domain.SocialPlatform;
 import org.crops.fitserver.auth.facade.AuthFacade;
 import org.crops.fitserver.auth.facade.dto.TokenResponse;
-import org.crops.fitserver.global.jwt.TokenCollection;
+import org.crops.fitserver.user.domain.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,16 +17,23 @@ import org.springframework.stereotype.Component;
 public class AuthFacadeImpl implements AuthFacade {
 
 	private final OAuthServiceProvider oAuthServiceProvider;
+	private final JwtProvider jwtProvider;
 
 	@Override
 	public TokenResponse socialLogin(String redirectUrl, String authorizationCode,
 			SocialPlatform socialPlatform) {
 		OAuthService oAuthService = oAuthServiceProvider.getOAuthService(socialPlatform);
-		TokenCollection tokenCollection = oAuthService
-				.socialLogin(
-						redirectUrl,
-						authorizationCode,
-						socialPlatform);
-		return TokenResponse.of(tokenCollection);
+		User user = oAuthService.socialUserLogin(
+				redirectUrl,
+				authorizationCode);
+		return TokenResponse.of(
+				jwtProvider.createTokenCollection(
+						TokenInfo.from(user)));
+	}
+
+	@Override
+	public SocialLoginPageResponse getSocialLoginPageUrl(SocialPlatform socialPlatform) {
+		OAuthService oAuthService = oAuthServiceProvider.getOAuthService(socialPlatform);
+		return SocialLoginPageResponse.from(oAuthService.getLoginPageUrl());
 	}
 }
