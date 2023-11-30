b8c764c4dfc5edf24ee70c7241787b98c95a25e9
유서린 <sprout3082@gmail.com>
2023-11-30 22:31:14 +0900

    #2 [refactor] kakao auth client header refactoring

--- src/main/java/org/crops/fitserver/global/feign/kakao/KakaoAuthServerClient.java
+++ src/main/java/org/crops/fitserver/global/feign/kakao/KakaoAuthServerClient.java
@@ -1,17 +1,19 @@
-package org.crops.fitserver.global.oauth;
+package org.crops.fitserver.global.feign.kakao;
 
+import org.crops.fitserver.global.feign.OAuthToken;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "kakaoAuthApiClient", url = "https://kauth.kakao.com")
-public interface Kakao {
+@FeignClient(name = "kakaoAuthServerClient", url = "https://kauth.kakao.com")
+public interface KakaoAuthServerClient {
 
 	@PostMapping(value = "/oauth/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	OAuthToken getOAuth2AccessToken(
-			@RequestHeader("content-type") String contentType,
+			@RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType,
 			@RequestParam("grant_type") String grantType,
 			@RequestParam("client_id") String clientId,
 			@RequestParam("redirect_uri") String redirectUri,
