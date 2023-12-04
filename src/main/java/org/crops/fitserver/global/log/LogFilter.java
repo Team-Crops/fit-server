66c0081eb1659fefdee7484fb22aa4b49f0b2e7c
9pruruluru2 <9pruruluru2@naver.com>
2023-12-04 17:09:59 +0900

    #5 [fix] package 변경

--- src/main/java/org/crops/fitserver/global/log/LogFilter.java
+++ src/main/java/org/crops/fitserver/global/log/LogFilter.java
@@ -1,11 +1,11 @@
 package org.crops.fitserver.global.log;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.jboss.logging.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
 
--- src/main/java/org/crops/fitserver/global/log/LogRequestFilter.java
+++ src/main/java/org/crops/fitserver/global/log/LogRequestFilter.java
@@ -1,11 +1,11 @@
 package org.crops.fitserver.global.log;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,6 +24,7 @@ public class LogRequestFilter extends OncePerRequestFilter {
         var requestURN = request.getRequestURI() + (request.getQueryString() != null ? "?"
                 + request.getQueryString() : "");
         log.info(requestMethod + " " + requestURN + ", requestBody: " + requestBody);
+
         filterChain.doFilter(request, response);
     }
 
