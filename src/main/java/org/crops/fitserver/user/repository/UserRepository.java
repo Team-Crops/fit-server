0108f9c039f7ee07a37b75aab90eeec68dd6d33b
유서린 <sprout3082@gmail.com>
2023-12-07 00:25:30 +0900

    #2 [refactor] UserRepositoryCustom 삭제

--- src/main/java/org/crops/fitserver/user/repository/UserRepository.java
+++ src/main/java/org/crops/fitserver/user/repository/UserRepository.java
@@ -5,8 +5,7 @@ import org.crops.fitserver.user.domain.User;
 import org.springframework.data.repository.Repository;
 
 public interface UserRepository extends
-    Repository<User, Long>,
-    UserRepositoryCustom {
+    Repository<User, Long> {
 
   Optional<User> findById(Long id);
 
