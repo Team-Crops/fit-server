80161a61415dc13b9aedc6f01c8c903d62de1cd1
유서린 <sprout3082@gmail.com>
2023-12-11 21:40:24 +0900

    #6 [feat] 환경변수 세팅 스크립트 적용

--- deploy/scripts/beforeInstall.sh
+++ deploy/scripts/beforeInstall.sh
@@ -1,5 +1,6 @@
 NOW_TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-
 echo "[$NOW_TIME] > Hello F-IT Deploy" >> /home/ubuntu/app/deploy.log
-echo "1 : $(ECR_REGISTRY)" >> /home/ubuntu/app/deploy.log
-echo "2 : [$ECR_REGISTRY]" >> /home/ubuntu/app/deploy.log
+echo "1 : $(ECR_USERNAME)" >> /home/ubuntu/app/deploy.log
+echo "2 : $(ECR_REGION)" >> /home/ubuntu/app/deploy.log
+echo "3 : $(ECR_REGISTRY)" >> /home/ubuntu/app/deploy.log
+echo "4 : [$ECR_REGISTRY]" >> /home/ubuntu/app/deploy.log
