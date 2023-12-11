ef3f866bd489c1718b4a787456cd6a54218661b7
유서린 <sprout3082@gmail.com>
2023-12-12 02:40:44 +0900

    #6 [feat] 배포 스크립트 추가

---
+++ deploy/scripts/healthCheck.sh
@@ -0,0 +1,30 @@
+NOW_TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    TARGET_PORT=8081
+else
+    echo "[$NOW_TIME] > No f-it WAS is connected to nginx port ${CURRENT_PORT}." >> /home/ubuntu/app/deploy.log
+    exit 1
+fi
+
+echo "[$NOW_TIME] > Start health check of f-it WAS at 'http://localhost:${TARGET_PORT}' ..." >> /home/ubuntu/app/deploy.log
+
+for RETRY_COUNT in {1..10}
+do
+    echo "[$NOW_TIME] > #${RETRY_COUNT} trying..." >> /home/ubuntu/app/deploy.log
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:${TARGET_PORT}/actuator/health)
+
+    if [ ${RESPONSE_CODE} -eq 200 ]; then
+        echo "[$NOW_TIME] > New f-it WAS successfully running" >> /home/ubuntu/app/deploy.log
+        exit 0
+    elif [ ${RETRY_COUNT} -eq 10 ]; then
+        echo "[$NOW_TIME] > Health check failed." >> /home/ubuntu/app/deploy.log
+        exit 1
+    fi
+    sleep 10
+done
---
+++ deploy/scripts/stopServer.sh
@@ -0,0 +1,22 @@
+NOW_TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+DEPRECATED_PORT=0
+DEPRECATED_STATE = ""
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    DEPRECATED_PORT = "8082"
+    DEPRECATED_STATE = "green"
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    DEPRECATED_PORT = "8081"
+    DEPRECATED_STATE = "blue"
+else
+    echo "[$NOW_TIME] > No f-it WAS is connected to nginx port ${CURRENT_PORT}." >> /home/ubuntu/app/deploy.log
+    exit 1
+fi
+
+if [ -z $(docker ps | grep fit-was-$(DEPRECATED_STATE)) ]; then
+  echo "[$NOW_TIME] > Kill f-it WAS running with state port ${DEPRECATED_PORT} in state $(DEPRECATED_STATE)." >> /home/ubuntu/app/deploy.log
+  docker-compose -f docker-compose.was.yml stop $(DEPRECATED_STATE)
+  docker rm fit-was-$(DEPRECATED_STATE)
+fi
