45573ad93e50e077ddca353c0eb892adfe4ea234
유서린 <sprout3082@gmail.com>
2023-12-13 15:11:41 +0900

    #6 [fix] code deploy 에러 수정 ( 현재 상태 변수 추가 )

--- deploy/scripts/runNewWas.sh
+++ deploy/scripts/runNewWas.sh
@@ -3,24 +3,24 @@ NOW_TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S
 CURRENT_PORT=$(cat /home/ubuntu/service-url.inc | grep -Po '[0-9]+' | tail -1)
 CURRENT_STATE=""
 TARGET_PORT=0
-TARGET_STATE = ""
+TARGET_STATE=""
 
 if [ $CURRENT_PORT -eq 8081 ]; then
-  CURRENT_STATE = "blue"
+  CURRENT_STATE="blue"
   TARGET_PORT=8082
-  TARGET_STATE = "green"
+  TARGET_STATE="green"
 elif [ $CURRENT_PORT -eq 8082 ]; then
-  CURRENT_STATE = "green"
+  CURRENT_STATE="green"
   TARGET_PORT=8081
-  TARGET_STATE = "blue"
+  TARGET_STATE="blue"
 else
   echo "[$NOW_TIME] > No f-it WAS is connected to nginx port $CURRENT_PORT." >> /home/ubuntu/app/deploy.log
   echo "[$NOW_TIME] > Assign current to port 8081 in blue state." >> /home/ubuntu/app/deploy.log
   CURRENT_PORT=8081
-  CURRENT_STATE = "blue"
+  CURRENT_STATE="blue"
   echo "set \$service_url http://localhost:$CURRENT_PORT;" | tee /home/ubuntu/service-url.inc
   TARGET_PORT=8082
-  TARGET_STATE = "green"
+  TARGET_STATE="green"
 fi
 
 echo "[$NOW_TIME] > Current status of running f-it WAS is port $CURRENT_PORT in $CURRENT_STATE state." >> /home/ubuntu/app/deploy.log
--- deploy/scripts/stopServer.sh
+++ deploy/scripts/stopServer.sh
@@ -2,14 +2,14 @@ NOW_TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S
 
 CURRENT_PORT=$(cat /home/ubuntu/service-url.inc | grep -Po '[0-9]+' | tail -1)
 DEPRECATED_PORT=0
-DEPRECATED_STATE = ""
+DEPRECATED_STATE=""
 
 if [ $CURRENT_PORT -eq 8081 ]; then
-    DEPRECATED_PORT = "8082"
-    DEPRECATED_STATE = "green"
+    DEPRECATED_PORT="8082"
+    DEPRECATED_STATE="green"
 elif [ $CURRENT_PORT -eq 8082 ]; then
-    DEPRECATED_PORT = "8081"
-    DEPRECATED_STATE = "blue"
+    DEPRECATED_PORT="8081"
+    DEPRECATED_STATE="blue"
 else
     echo "[$NOW_TIME] > No f-it WAS is connected to nginx port $CURRENT_PORT." >> /home/ubuntu/app/deploy.log
     exit 1
