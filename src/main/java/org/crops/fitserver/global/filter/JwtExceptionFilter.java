cb86711175e2d1c9efeb8ff85706a50878152392
9pruruluru2 <9pruruluru2@naver.com>
2023-12-20 00:47:56 +0900

    #1 [fix] jwtExceptionFilter 주석 제거

--- src/main/java/org/crops/fitserver/global/filter/JwtExceptionFilter.java
+++ src/main/java/org/crops/fitserver/global/filter/JwtExceptionFilter.java
@@ -1,61 +1,61 @@
-//package org.crops.fitserver.global.filter;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.crops.fitserver.global.exception.FitException;
-//import org.crops.fitserver.global.http.ErrorType;
-//import org.crops.fitserver.global.http.FailResponse;
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//@Slf4j
-//@Component
-//@RequiredArgsConstructor
-//public class JwtExceptionFilter extends OncePerRequestFilter {
-//
-//  private final ObjectMapper objectMapper;
-//
-//  @Override
-//  protected void doFilterInternal(
-//      HttpServletRequest request,
-//      HttpServletResponse response,
-//      FilterChain filterChain) throws ServletException, IOException {
-//    try {
-//      filterChain.doFilter(request, response);
-//    } catch (FitException e) {
-//      log.error("error : {}", e.getMessage(), e);
-//      responseError(
-//          response,
-//          e.getErrorType(),
-//          e.getErrorType().getMessage());
-//    } catch (Exception e) {
-//      log.error("error : {}", e.getMessage(), e);
-//      responseError(
-//          response,
-//          ErrorType.INTERNAL_SERVER_ERROR,
-//          ErrorType.INTERNAL_SERVER_ERROR.getMessage());
-//    }
-//  }
-//
-//  private void responseError(
-//      HttpServletResponse response,
-//      ErrorType errorType,
-//      String message)
-//      throws IOException {
-//    response.setStatus(errorType.getStatusCode());
-//    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//    response.setCharacterEncoding("UTF-8");
-//    response.getWriter()
-//        .write(objectMapper.writeValueAsString(
-//            new FailResponse(
-//                errorType.getStatusCode(),
-//                message)));
-//  }
-//}
+package org.crops.fitserver.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.crops.fitserver.global.exception.BusinessException;
+import org.crops.fitserver.global.exception.ErrorCode;
+import org.crops.fitserver.global.exception.ErrorResponse;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    try {
+      filterChain.doFilter(request, response);
+    } catch (BusinessException e) {
+      log.error("error : {}", e.getMessage(), e);
+      responseError(
+          response,
+          e.getErrorCode(),
+          e.getErrorCode().getMessage());
+    } catch (Exception e) {
+      log.error("error : {}", e.getMessage(), e);
+      responseError(
+          response,
+          ErrorCode.INTERNAL_SERVER_ERROR,
+          ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+    }
+  }
+
+  private void responseError(
+      HttpServletResponse response,
+      ErrorCode errorCode,
+      String message)
+      throws IOException {
+    response.setStatus(errorCode.getHttpStatus().value());
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter()
+        .write(objectMapper.writeValueAsString(
+            new ErrorResponse(
+                errorCode.getCode(),
+                message)));
+  }
+}
