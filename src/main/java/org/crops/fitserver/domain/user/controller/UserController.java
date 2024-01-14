7eeb8fde30b6c813c324ca8d6069dedb8bca626b
9pruruluru2 <9pruruluru2@naver.com>
2024-01-15 06:37:56 +0900

    [fix] user관련 controller 뼈대 추가

--- src/main/java/org/crops/fitserver/domain/user/controller/UserController.java
+++ src/main/java/org/crops/fitserver/domain/user/controller/UserController.java
@@ -3,12 +3,14 @@ package org.crops.fitserver.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.crops.fitserver.domain.user.dto.UserInfoDto;
+import org.crops.fitserver.domain.user.dto.request.UpdateUserRequest;
 import org.crops.fitserver.domain.user.facade.UserFacade;
 import org.crops.fitserver.global.annotation.V1;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @V1
@@ -21,7 +23,12 @@ public class UserController {
   private final UserFacade userFacade;
 
   @GetMapping()
-  public ResponseEntity<UserInfoDto> getUserInfo(@AuthenticationPrincipal Long userId) {
+  public ResponseEntity<UserInfoDto> getUser(@AuthenticationPrincipal Long userId) {
     return ResponseEntity.ok(userFacade.getUserWithInfo(userId));
+  }
+
+  @PutMapping()
+  public ResponseEntity<UserInfoDto> updateUser(@AuthenticationPrincipal Long userId, UpdateUserRequest updateUserRequest) {
+    return ResponseEntity.ok(userFacade.updateUserWithInfo(userId, updateUserRequest));
   }
 }
