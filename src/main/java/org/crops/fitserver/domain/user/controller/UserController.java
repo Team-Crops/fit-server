913e49a3c1d79ab1fd81512c155f7deea45a44d8
9pruruluru2 <9pruruluru2@naver.com>
2024-01-24 15:29:52 +0900

    #25 [refactor] validation 추가 및 openapi server 수정

--- src/main/java/org/crops/fitserver/domain/user/controller/UserController.java
+++ src/main/java/org/crops/fitserver/domain/user/controller/UserController.java
@@ -1,16 +1,14 @@
 package org.crops.fitserver.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.crops.fitserver.domain.user.domain.User;
 import org.crops.fitserver.domain.user.dto.UserInfoDto;
 import org.crops.fitserver.domain.user.dto.request.UpdateUserRequest;
 import org.crops.fitserver.domain.user.facade.UserFacade;
 import org.crops.fitserver.global.annotation.CurrentUserId;
 import org.crops.fitserver.global.annotation.V1;
-import org.crops.fitserver.global.security.PrincipalDetails;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +32,7 @@ public class UserController {
   @PutMapping()
   public ResponseEntity<UserInfoDto> updateUser(
       @CurrentUserId Long userId,
-      UpdateUserRequest updateUserRequest) {
+      @Valid UpdateUserRequest updateUserRequest) {
     return ResponseEntity.ok(userFacade.updateUserWithInfo(userId, updateUserRequest));
   }
 }
