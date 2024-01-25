6b6d15b0f56e9e8995326035fb9ff3bb7c2f7257
유서린 <sprout3082@gmail.com>
2024-01-25 22:38:08 +0900

    #24 [fix] cd pipeline test

--- deploy/scripts/stopServer.sh
+++ deploy/scripts/stopServer.sh
@@ -1,13 +1,8 @@
 source /etc/environment
-
-echo "[$TIME] > 1-1-1-1-1-1-1-1-1-." >> /home/ubuntu/app/deploy/deploy.log
-
 TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
 CURRENT_PORT=$(cat /home/ubuntu/service-url.inc | grep -Po '[0-9]+' | tail -1)
 DEPRECATED_PORT=0
 DEPRECATED_STATE=""
-
-echo "[$TIME] > 2-2-2-2-2-2-2-2-2-22. $CURRENT_PORT. " >> /home/ubuntu/app/deploy/deploy.log
 
 if [ $CURRENT_PORT -eq 8081 ]; then
     DEPRECATED_PORT="8082"
@@ -20,9 +15,6 @@ else
     exit 1
 fi
 
-echo "[$TIME] > 3-3-3-3-3-3-3-3-3-333-33-33. $DEPRECATED_PORT  " >> /home/ubuntu/app/deploy/deploy.log
-echo "[$TIME] > 3-3-3-3-3-3-3-3-3-333-33-33. $DEPRECATED_STATE  " >> /home/ubuntu/app/deploy/deploy.log
-
 if [ -n "$(docker ps | grep fit-was-$DEPRECATED_STATE)" ]; then
   echo "[$TIME] > Kill f-it WAS running with state port $DEPRECATED_PORT in $DEPRECATED_STATE state." >> /home/ubuntu/app/deploy/deploy.log
   docker-compose -f /home/ubuntu/app/deploy/docker/docker-compose.was.yml stop $DEPRECATED_STATE
@@ -33,5 +25,3 @@ if [ -n "$(docker images -q 728702143069.dkr.ecr.ap-northeast-2.amazonaws.com/fi
   echo "[$TIME] > Remove WAS images not latest version." >> /home/ubuntu/app/deploy/deploy.log
   docker rmi -f $(docker images -a 728702143069.dkr.ecr.ap-northeast-2.amazonaws.com/fit-was --filter "before=728702143069.dkr.ecr.ap-northeast-2.amazonaws.com/fit-was:$ENV" -q)
 fi
-
-echo "[$TIME] > 4-4-4-4-4-4-4-4-4-4-4-4--4444 " >> /home/ubuntu/app/deploy/deploy.log
