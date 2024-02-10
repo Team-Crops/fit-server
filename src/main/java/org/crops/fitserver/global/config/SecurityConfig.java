ffd29b4fcb10abaa7607cfeda8c151626f538679
9pruruluru2 <9pruruluru2@naver.com>
2024-02-10 17:49:39 +0900

    [fix] annotation 기반 spring security가 동작하도록 수정

--- src/main/java/org/crops/fitserver/global/config/SecurityConfig.java
+++ src/main/java/org/crops/fitserver/global/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.crops.fitserver.global.jwt.JwtResolver;
 import org.crops.fitserver.global.security.PrincipalDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +23,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
