af9118449425776a7e52bd5b0df7ccc96e516ff9
유서린 <sprout3082@gmail.com>
2024-02-28 15:59:27 +0900

    #24 [fix] log message 수정

--- deploy/scripts/buildInfra.sh
+++ deploy/scripts/buildInfra.sh
@@ -2,12 +2,12 @@ source /etc/environment
 TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
 
 
-if [ -n "$(docker ps | grep fit-mariadb)" ]; then
+if [ -z "$(docker ps | grep fit-mariadb)" ]; then
   echo "[$TIME] > Start running f-it Mariadb." >> /home/ubuntu/app/deploy/deploy.log
   docker-compose -f /home/ubuntu/app/deploy/docker/docker-compose.infra.yml pull mariadb \
   && docker-compose -f /home/ubuntu/app/deploy/docker/docker-compose.infra.yml up -d mariadb
 else
-  echo "[$TIME] > f-it Redis is already running." >> /home/ubuntu/app/deploy/deploy.log
+  echo "[$TIME] > f-it MariaDB is already running." >> /home/ubuntu/app/deploy/deploy.log
 fi
 
 if [ -z "$(docker ps | grep fit-redis)" ]; then # redis 가 돌아가지 않고 있음
