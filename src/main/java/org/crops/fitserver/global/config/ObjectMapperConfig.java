48a5e6c017fbc5f12eed229dcbb5909558241710
9pruruluru2 <9pruruluru2@naver.com>
2024-03-11 14:50:10 +0900

    #40 [fix] LocalDateTime 객체의 역직렬화가 안되는 버그 수정

--- src/main/java/org/crops/fitserver/global/config/ObjectMapperConfig.java
+++ src/main/java/org/crops/fitserver/global/config/ObjectMapperConfig.java
@@ -3,6 +3,11 @@ package org.crops.fitserver.global.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +25,13 @@ public class ObjectMapperConfig {
       builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
       builder.featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
       builder.featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-      builder.modules(jsonNullableModule());
+
+      final SimpleModule localDateTimeModule = new SimpleModule();
+      localDateTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(
+          DateTimeFormatter.ISO_DATE_TIME));
+      localDateTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(
+          DateTimeFormatter.ISO_DATE_TIME));
+      builder.modules(jsonNullableModule(), localDateTimeModule);
     };
   }
 
