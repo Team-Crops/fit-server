f1f0d9cf1f33a18e86f3861dbb0b3848f9c1f263
유서린 <sprout3082@gmail.com>
2024-03-17 14:20:00 +0900

    #39 [feat] add Data sql

--- src/main/resources/db/mariadb/data.sql
+++ src/main/resources/db/mariadb/data.sql
@@ -1,8 +1,12 @@
--- Local User Data
+-- User Data
 insert into user
 (user_role, is_open_phone_num, created_at, updated_at, is_deleted)
-values ("ADMIN", true, now(), now(), false),
-       ("MANAGER", true, now(), now(), false),
-       ("MEMBER", true, now(), now(), false),
-       ("MEMBER", false, now(), now(), false),
-         ("NON_MEMBER", false, now(), now(), false);
+values ('ADMIN', true, now(), now(), false)
+;
+
+-- Social User Info Data
+insert into social_user_info
+(user_id, social_type, social_code)
+values (1, 'KAKAO', 'KAKAO_3169170192')
+;
+
