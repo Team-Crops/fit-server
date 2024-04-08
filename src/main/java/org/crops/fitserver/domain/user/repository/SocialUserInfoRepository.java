e3e11c75cfb1cdc9b44354b225cc06c3daaaf5a5
유서린 <sprout3082@gmail.com>
2024-04-08 13:42:10 +0900

    #39 [fix] 소셜 로그인 UserFetchJoin

--- src/main/java/org/crops/fitserver/domain/user/repository/SocialUserInfoRepository.java
+++ src/main/java/org/crops/fitserver/domain/user/repository/SocialUserInfoRepository.java
@@ -2,11 +2,15 @@ package org.crops.fitserver.domain.user.repository;
 
 import java.util.Optional;
 import org.crops.fitserver.domain.user.domain.SocialUserInfo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 public interface SocialUserInfoRepository extends
     Repository<SocialUserInfo, Long> {
 
+  @Query("select s from SocialUserInfo s "
+      + "left join fetch s.user u "
+      + "where s.socialCode = :socialCode")
   Optional<SocialUserInfo> findBySocialCode(String socialCode);
 
   SocialUserInfo save(SocialUserInfo socialUserInfo);
