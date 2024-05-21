53b7c119aa1b5c63c6000fda1f8bb3954c90d8f0
유서린 <sprout3082@gmail.com>
2024-05-22 03:46:30 +0900

    #63 [feat] 유저 조회, 유저 탈퇴 API 추가

--- src/main/java/org/crops/fitserver/domain/user/controller/UserController.java
+++ src/main/java/org/crops/fitserver/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.crops.fitserver.domain.user.dto.UserInfoDto;
 import org.crops.fitserver.domain.user.dto.request.UpdatePolicyAgreementRequest;
 import org.crops.fitserver.domain.user.dto.request.UpdateUserRequest;
 import org.crops.fitserver.domain.user.dto.response.GetPolicyAgreementResponse;
+import org.crops.fitserver.domain.user.dto.response.GetUserStatusAndInfoResponse;
 import org.crops.fitserver.domain.user.dto.response.UpdatePolicyAgreementResponse;
 import org.crops.fitserver.domain.user.facade.UserFacade;
 import org.crops.fitserver.global.annotation.CurrentUserId;
@@ -15,8 +16,10 @@ import org.crops.fitserver.global.annotation.V1;
 import org.crops.fitserver.global.exception.BusinessException;
 import org.crops.fitserver.global.exception.ErrorCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,14 @@ public class UserController {
   public ResponseEntity<UserInfoDto> getUser(
       @CurrentUserId Long userId) {
     return ResponseEntity.ok(userFacade.getUserWithInfo(userId));
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<GetUserStatusAndInfoResponse> getUserStatusAndInfoById(
+      @CurrentUserId Long loginUserId,
+      @PathVariable(name = "userId") Long userId) {
+    return ResponseEntity.ok(
+        userFacade.getUserStatusAndInfo(loginUserId, userId));
   }
 
   @PatchMapping()
@@ -64,5 +75,12 @@ public class UserController {
     return ResponseEntity.ok(
         UpdatePolicyAgreementResponse.of(userFacade.updatePolicyAgreement(userId,
             updatePolicyAgreementRequest.policyAgreementList())));
+  }
+
+  @DeleteMapping()
+  public ResponseEntity<Void> deleteUser(
+      @CurrentUserId long userId) {
+    userFacade.deleteUser(userId);
+    return ResponseEntity.noContent().build();
   }
 }
