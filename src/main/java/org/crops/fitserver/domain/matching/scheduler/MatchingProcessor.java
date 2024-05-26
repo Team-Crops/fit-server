4f78e9f04377567ceac3da11cedddcdd3a6e207c
9pruruluru2 <9pruruluru2@naver.com>
2024-05-26 16:49:01 +0900

    [fix] 인덱스 추가

--- src/main/java/org/crops/fitserver/domain/matching/scheduler/MatchingProcessor.java
+++ src/main/java/org/crops/fitserver/domain/matching/scheduler/MatchingProcessor.java
@@ -7,6 +7,7 @@ import static org.crops.fitserver.domain.matching.VO.ComparableMatchingParameter
 import static org.crops.fitserver.domain.matching.constant.MatchingConstants.MINIMUM_REQUIRED_POSITIONS;
 
 import io.jsonwebtoken.lang.Collections;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -42,7 +43,7 @@ public class MatchingProcessor {
   @Transactional
   public void match() {
 
-    var matchingRoomList = matchingRoomRepository.findMatchingRoomNotComplete();
+    var matchingRoomList = matchingRoomRepository.findMatchingRoomNotComplete(LocalDateTime.now().minusDays(5));
     var matchingList = matchingRepository.findMatchingWithoutRoom();
 
     var matchingMap = getMatchingMap(matchingList);
